06f3a15f0152b495be52ce52358761381a2b9765
suns <suns@firsov.net>
2021-06-06 01:10:06 -0700

    * added demo/index.html * support for template * support for slots: source, demo, text

--- html-demo-element.js
+++ html-demo-element.js
@@ -2,15 +2,15 @@ import "https://unpkg.com/prismjs@1.23.0/components/prism-core.min.js";
 import "https://unpkg.com/prismjs@1.23.0/components/prism-markup.min.js";
 import "https://unpkg.com/prismjs@1.23.0/plugins/autoloader/prism-autoloader.min.js";
 
-var styles = `
+const createCss = ( text, el = document.createElement( "style" ) ) =>
+{   el.type = "text/css";
+    el.innerText = text;
+    document.head.appendChild( el );
+};
+createCss(`
     @import "https://unpkg.com/prismjs@1.23.0/themes/prism.css";
     html-demo-element{ display: block; border: blueviolet dashed 1px; border-radius: 1rem; padding: 1rem; margin: 1rem; }
-`
-
-const styleSheet = document.createElement("style")
-styleSheet.type = "text/css"
-styleSheet.innerText = styles
-document.head.appendChild(styleSheet);
+`);
 
 for( let el of document.querySelectorAll('html-demo-element') )
     el.initialHTML = el.innerHTML;
@@ -19,10 +19,31 @@ for( let el of document.querySelectorAll('html-demo-element') )
 HtmlDemoElement extends HTMLElement
 {
     connectedCallback()
-    {   const  html = Prism.highlight( this.initialHTML, Prism.languages.html, 'html' )
-        , pre = document.createElement( 'div' )
+    {   const      $ = x => this.querySelector(x)
+        ,   replaceDom = ( parent, child ) => { parent.innerHTML = ''; parent.append(child); }
+        ,   template = $('[slot="source"]') || $('template')
+        ,    srcText = template ? template.innerHTML :  this.initialHTML;
+
+        const  html = Prism.highlight( srcText, Prism.languages.html, 'html' )
+        ,       pre = document.createElement( 'div' )
         pre.innerHTML = `<h3>${this.title||''}</h3><pre><code class="language-markup" >${html}</code></pre>`;
-        this.insertBefore( pre, this.firstChild );
+        if( template )
+        {
+            const textSlot = $('[slot="text"]');
+            if( textSlot )
+                replaceDom( textSlot, pre );
+            else
+            {   const ref = template.nextElementSibling || template.parentElement.lastElementChild;
+                template.parentElement.insertBefore( pre, ref );
+            }
+            const demoDom = template.content.cloneNode( true )
+            ,        demo = $('[slot="demo"]')
+            if( demo )
+                replaceDom( demo, demoDom );
+            else
+                template.parentElement.insertBefore( demoDom, ref );
+        }else
+            this.insertBefore( pre, this.firstChild );
     }
 }
 window.customElements.define( 'html-demo-element', HtmlDemoElement);
